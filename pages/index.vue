8593f7bfda879259d7762f6dfebe8ae5be124ecf
evelinasundin <evelinasundin@hotmail.se>
2020-10-08 20:36:16 +0200

    test and see if vercel can handle scss

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,22 @@
       <p class="text-white font-serif">
         Let's see if Vercel can handle Tailiwnd
       </p>
+      <p class="test"> Let's see if Vercel can handle SCSS </p>
     </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+.test {
+  &:before {
+    content: '';
+    position: absolute;
+    height: 10px;
+    width: 10px;
+    background-color: red;
+  }
+}
+</style>
